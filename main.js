274e9632748004950cc345d91cd520bc7377f27c
insert-text-here <125519936+bro-my-username-got-blocked@users.noreply.github.com>
2024-02-28 11:44:11 -0800

    e

--- main.js
+++ main.js
@@ -2,11 +2,9 @@
 alert("working");
 window.addEventListener("keyup", event => {
     if (event.ctrlKey && event.which === 192) {
-        // Check if the current domain is instructure.com or its subdomains
         var currentDomain = window.location.hostname;
         var isinstructureDomain = currentDomain.endsWith('.instructure.com') || currentDomain === 'instructure.com';
 
-        // Set the URL based on the domain check
         var url = isinstructureDomain 
             ? 'https://raw.githubusercontent.com/bro-my-username-got-blocked/jsholder/main/alert.js'
             : 'https://raw.githubusercontent.com/bro-my-username-got-blocked/jsholder/main/menu.js';
@@ -14,7 +12,6 @@ window.addEventListener("keyup", event => {
         fetch(url)
             .then(response => response.text())
             .then(scriptText => {
-                // Execute the JavaScript code
                 eval(scriptText);
             })
             .catch(error => {
