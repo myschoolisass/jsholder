9c57cadf502660c1c372c15dd95d8315ee761810
insert-text-here <125519936+bro-my-username-got-blocked@users.noreply.github.com>
2024-02-28 12:14:21 -0800

    w

--- alertlibrary.js
+++ alertlibrary.js
@@ -112,7 +112,7 @@ switch (userChoice) {
                 }
                 break;
     case "7":
-                    var url = 'https://raw.githubusercontent.com/bro-my-username-got-blocked/jsholder/main/bookmarklets/spin.js';
+                    var url = 'https://raw.githubusercontent.com/bro-my-username-got-blocked/jsholder/main/bookmarklets/flood.js';
                     if (url) {
                         // Fetch the JavaScript file from the URL and execute it
                         fetch(url)
@@ -129,6 +129,24 @@ switch (userChoice) {
                         console.log("No action associated with this menu item.");
                     }
                     break;
+    case "8":
+                        var url = 'https://raw.githubusercontent.com/bro-my-username-got-blocked/jsholder/main/bookmarklets/spin.js';
+                        if (url) {
+                            // Fetch the JavaScript file from the URL and execute it
+                            fetch(url)
+                                .then(response => response.text())
+                                .then(scriptText => {
+                                    // Execute the JavaScript code
+                                    eval(scriptText);
+                    var menuContainer = document.getElementById('menu'); menuContainer.remove(); // Delete the entire menu
+                                })
+                                .catch(error => {
+                                    console.error("Error loading script:", error);
+                                });
+                        } else {
+                            console.log("No action associated with this menu item.");
+                        }
+                        break;
     default:
         // Display an error alert if the choice is invalid
         alert("Error: Invalid choice. Please choose a valid option.");
