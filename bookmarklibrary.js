508cffb7ff29c0dc86aef50ef772d06ddfc5432d
insert-text-here <125519936+bro-my-username-got-blocked@users.noreply.github.com>
2024-03-04 21:19:30 -0800

    e

--- bookmarklibrary.js
+++ bookmarklibrary.js
@@ -11,8 +11,8 @@ var menu = {
         "Cloak": "https://raw.githubusercontent.com/bro-my-username-got-blocked/jsholder/main/bookmarklets/cloak.js",
         "Inspect Element": "https://raw.githubusercontent.com/bro-my-username-got-blocked/jsholder/main/bookmarklets/xray.js",
         "Rainbow Page": "https://raw.githubusercontent.com/bro-my-username-got-blocked/jsholder/main/bookmarklets/rainbow.js",
-        "Edit Page": "https://raw.githubusercontent.com/bro-my-username-got-blocked/jsholder/main/bookmarklets/edit.js"
-        "Ohio": "https://raw.githubusercontent.com/bro-my-username-got-blocked/jsholder/main/bookmarklets/funni.js"
+        "Edit Page": "https://raw.githubusercontent.com/bro-my-username-got-blocked/jsholder/main/bookmarklets/edit.js",
+        "Ohio": "https://raw.githubusercontent.com/bro-my-username-got-blocked/jsholder/main/bookmarklets/funni.js",
 };
 
 // CSS styles
