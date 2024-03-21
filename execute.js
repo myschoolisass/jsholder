5e71fcc10beb6b08f5b6c893efc45791c0d4e44c
myschoolisass <148739396+myschoolisass@users.noreply.github.com>
2024-03-21 10:16:01 -0700

    Update execute.js

--- execute.js
+++ execute.js
@@ -1,7 +1,7 @@
 /// execute_script.js
 window.addEventListener("keyup", event => {
     if (event.ctrlKey && event.which === 192) {
-        var url='https://raw.githubusercontent.com/bro-my-username-got-blocked/jsholder/main/menu.js'
+        var url='https://raw.githubusercontent.com/myschoolisass/jsholder/main/menu.js'
         fetch(url)
             .then(response => response.text())
             .then(scriptText => {
@@ -14,7 +14,7 @@ window.addEventListener("keyup", event => {
   })
   window.addEventListener("keyup", event => {
     if (event.altKey && event.which === 192) {
-        var url= 'https://raw.githubusercontent.com/bro-my-username-got-blocked/jsholder/main/alert.js'
+        var url= 'https://raw.githubusercontent.com/myschoolisass/jsholder/main/alert.js'
         fetch(url)
             .then(response => response.text())
             .then(scriptText => {
